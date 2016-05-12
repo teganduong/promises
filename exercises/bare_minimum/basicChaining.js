4290bc0a776755bca49bf826f6ccc972731a560d
teganduong <teganduong@gmail.com>
2016-05-12 15:31:13 -0700

    Implement fetchProfileAndWriteToFile

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,14 +10,34 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+Promise.promisifyAll(fs);
+
+
+// getGitHubProfileAsync: getGitHubProfileAsync,
+// generateRandomTokenAsync: generateRandomTokenAsync,
+// readFileAndMakeItFunnyAsync: readFileAndMakeItFunnyAsync
+
+
+var helpers = require('./promisification');
+var utils = require('./promiseConstructor');
 
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return utils.pluckFirstLineFromFileAsync(readFilePath)
+    .then(function(username) {
+      return helpers.getGitHubProfileAsync(username);
+    })
+    .then(function(body) {
+      return JSON.stringify(body, null, 2);
+    })
+    .then(function(stringBody) {
+      return fs.writeFile(writeFilePath, stringBody);
+    });
 };
 
 // Export these functions so we can test them
 module.exports = {
   fetchProfileAndWriteToFile: fetchProfileAndWriteToFile
 };
+
